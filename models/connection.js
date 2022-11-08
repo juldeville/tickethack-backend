33af91eee50bd94edfa62d122ba1f0a4611c802f
juldeville <j.deville@protonmail.com>
2022-11-08 12:20:33 +0100

    added mongoose, trips route, trips model

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://raindear:2qduliiiuQSgzx!7@cluster0.eu0esgp.mongodb.net/tickethack'
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000})
+    .then(() => console.log('Database connected'))
+    .catch(error => console.error(error))
